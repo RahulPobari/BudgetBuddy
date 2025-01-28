c2daa2d2a546d8770d0c9106ae0823a901ef65d2
RahulPobari <pobarirahul134@gmail.com>
2025-01-28 23:22:29 +0530

    code updated

--- components/ScreenWrapper.tsx
+++ components/ScreenWrapper.tsx
@@ -1,31 +1,3 @@
-// import { Dimensions, Platform, StatusBar, StyleSheet, Text, View } from 'react-native'
-// import React from 'react'
-// import { ScreenWrapperProps } from '@/types'
-// import { colors } from '@/constants/theme'
-
-
-// const {height} = Dimensions.get('window')
-
-// const ScreenWrapper = ({style, children}: ScreenWrapperProps) => {
-//     let paddingTop = Platform.OS == 'ios' ? height * 0.06 : 50;
-//   return (
-//     <View style={[{
-//         paddingTop,
-//         flex: 1,
-//         backgroundColor: colors.neutral900,
-//     },style
-//     ]}>
-//       <StatusBar barStyle="light-content" />
-//     {children}
-//     </View>
-//   )
-// }
-
-// export default ScreenWrapper
-
-// const styles = StyleSheet.create({})
-
-
 import React from 'react';
 import { StatusBar, StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
