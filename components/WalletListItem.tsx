c28fd715b4d6520f86f95d46774041ada55de92d
RahulPobari <pobarirahul134@gmail.com>
2025-01-31 17:44:36 +0530

    code updated

--- components/WalletListItem.tsx
+++ components/WalletListItem.tsx
@@ -1,8 +1,10 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
 import Typo from './Typo'
 import { WalletType } from '@/types'
 import { Router } from 'expo-router'
+import { verticalScale } from '@/utils/styling'
+import { colors, radius, spacingX } from '@/constants/theme'
 
 const WalletListItem = ({
     item,
@@ -15,11 +17,33 @@ const WalletListItem = ({
 }) => {
   return (
     <View>
-      <Typo>WalletListItem</Typo>
+        <TouchableOpacity>
+
+        </TouchableOpacity>
     </View>
   )
 }
 
 export default WalletListItem
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginBottom: verticalScale(17),
+    },
+    imageContainer: {
+        width: verticalScale(45),
+        height: verticalScale(45),
+        borderWidth: 1,
+        borderColor: colors.neutral600,
+        borderRadius: radius._12,
+        borderCurve: 'continuous',
+        overflow: 'hidden',
+    },
+    nameContainer: {
+        flex: 1,
+        gap: 2,
+        marginLeft: spacingX._10,
+    }
+})
