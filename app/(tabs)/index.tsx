2fd0e3123243e802d08af4f99a26e071ea2d387a
RahulPobari <pobarirahul134@gmail.com>
2025-02-08 19:42:46 +0530

    code updated

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,22 +2,60 @@ import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import Button from '@/components/Button'
 import Typo from '@/components/Typo'
-import { colors } from '@/constants/theme'
+import { colors, spacingX, spacingY } from '@/constants/theme'
 import { signOut } from 'firebase/auth'
 import { auth } from '@/config/firebase'
 import { useAuth } from '@/contexts/authContext'
 import ScreenWrapper from '@/components/ScreenWrapper'
+import { verticalScale } from '@/utils/styling'
 
 const Home = () => {
     const { user } = useAuth();
-    // console.log("user: ", user);
     return (
         <ScreenWrapper>
-            <Typo>Home</Typo>
+            <View style={styles.container}>
+
+                {/* Header */}
+                <View style={styles.header}>
+                    <View style={{gap: 4}}>
+                        <Typo>Hello,</Typo>
+                    </View>
+                </View>
+            </View>
         </ScreenWrapper>
     )
 }
 
 export default Home
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        paddingHorizontal: spacingX._20,
+        marginTop: verticalScale(8)
+    },
+    header: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginBottom: spacingY._10
+    },
+    searchIcon: {
+        backgroundColor: colors.neutral700,
+        padding: spacingX._10,
+        borderRadius: 50,
+    },
+    floatingButton: {
+        height: verticalScale(50),
+        width: verticalScale(50),
+        borderRadius: 100,
+        position: 'absolute',
+        bottom: verticalScale(30),
+        right: verticalScale(30),
+    },
+    scrollViewStyle: {
+        marginTop: spacingY._10,
+        paddingBottom: verticalScale(100),
+        gap: spacingY._25
+    }
+})
